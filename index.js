73e84acae454c2dcc66ec871ee5bc1494a9fdd68
Marwan Zaky <marouaneezzaky@gmail.com>
2022-08-22 23:49:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,27 +16,21 @@ const productDataObj = JSON.parse(productDataJson);
 const instagramDataJson = fs.readFileSync(`${__dirname}/data/instagram-data.json`);
 const instagramDataObj = JSON.parse(instagramDataJson);
 
-const getAllProducts = (req, res) => {
+router.get('/products', (req, res) => {
     res.status(200).json(productDataObj);
-};
+});
 
-const getProductImg = (req, res) => {
+router.get('/img/products/:name/:index', (req, res) => {
     res.status(200).sendFile(`${__dirname}/img/products/${req.params.name}/img-${req.params.index}.png`);
-};
+});
 
-const getInstagram = (req, res) => {
+router.get('/instagram', (req, res) => {
     res.status(200).json(instagramDataObj);
-};
+});
 
-const getInstaImg = (req, res) => {
+router.get('/img/instagram/:name', (req, res) => {
     res.status(200).sendFile(`${__dirname}/img/instagram/${req.params.name}`);
-}
-
-router.get('/products', getAllProducts);
-router.get('/img/products/:name/:index', getProductImg);
-
-router.get('/instagram', getInstagram);
-router.get('/img/instagram/:name', getInstaImg);
+});
 
 app.use('/', router);
 
