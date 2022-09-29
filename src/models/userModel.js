0e2cf49f4afcc125a56baaab2753fdf6e0ee8114
Marwan Zaky <marouaneezzaky@gmail.com>
2022-09-30 00:46:18 +0100

    userModel.js bug fixed

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -67,13 +67,11 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     return false;
 }
 
-userSchema.methods.createPasswordResetToken = async function () {
+userSchema.methods.createPasswordResetToken = function () {
     const resetToken = crypto.randomBytes(32).toString('hex');
 
     this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-    console.log(resetToken, this.passwordResetToken);
 
     return resetToken;
 }
