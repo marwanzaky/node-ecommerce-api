d86a3543f93ace4d90af4c5b173ec321a76fc77d
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-07 15:07:11 +0100

    Convert png images to jpg to reduce the size significantly

--- src/controllers/imgController.js
+++ src/controllers/imgController.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 exports.getImgId = (req, res) => {
-    const pathImgId = path.resolve(__dirname, `../../img/products/${req.params.name}/img-${req.params.index}.png`);
+    const pathImgId = path.resolve(__dirname, `../../img/products/${req.params.name}/img-${req.params.index}.jpg`);
     res.status(200).sendFile(pathImgId);
 }
