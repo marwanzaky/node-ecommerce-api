50001b7e0a118acdda2e237cbf054d01f549d877
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-12 23:44:58 +0100

    Send a JWT response as a cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,18 @@ const signToken = (id) => {
 }
 
 const createSendToken = (res, user, statusCode) => {
+    user.password = undefined;
+
     const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production')
+        cookieOptions.secure = true;
+
+    res.cookie('jwk', token, cookieOptions);
 
     res.status(statusCode).json({
         status: 'success',
