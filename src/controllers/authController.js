81c71602e44c60479ff9382f32df931126a9bd1a
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-22 20:45:37 +0100

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,6 +15,7 @@ const createSendToken = (res, user, statusCode) => {
 
     const cookieOptions = {
         // expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        maxAge: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production'
     };
