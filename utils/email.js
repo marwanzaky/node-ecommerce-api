0b65e0c5040d6e0a42a730578e24981794c5f4ae
Marwan Zaky <marouaneezzaky@gmail.com>
2023-03-20 23:31:52 +0000

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
-const pug = require('pug');
 const htmlToText = require('html-to-text');
+const pug = require('pug');
 
 module.exports = class Email {
     constructor(user, url) {
